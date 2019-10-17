45b0cc189595f7a0d8ad1f581d07c1aabc0dd2dd
GJL <55914881+orionGGG@users.noreply.github.com>
2019-10-17 23:35:34 +0800

    Update float-equal.c
    GO

--- float-equal.c
+++ float-equal.c
@@ -4,7 +4,9 @@ int main()
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
-    if (z-z1 < 0.00001 && z-z1 > -0.00001) {
+    int k = (z*100000+0.5);
+    int k1 = (z1*100000+0.5);
+    if (k == k1) {
         z = (m + x) / y;
     }
     printf("Z is  %f\n", z);
