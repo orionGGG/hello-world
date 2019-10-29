4656a98a30746bc41fc6821f21311a0079848daa
GJL <55914881+orionGGG@users.noreply.github.com>
2019-10-29 13:08:40 +0800

    Revert "123"
    This reverts commit 2a808e5b1c9ef8cb3ae4823f20c7f615a8749e43.

--- sort_by_exchange.cpp
+++ sort_by_exchange.cpp
@@ -1,15 +1,19 @@
+//交换排序
 #include <stdio.h>
 int main() {
-  int x[3] = {5, 2, 1}, i,j;
-    for(j=0;j<3;j++){
-    	for(i=j;i<2;i++) {
-      		if( x[j] > x[i+1]) {
-  	    		int temp = x[j];
-  	    		x[j] = x[i+1];
-  	    		x[i+1] = temp;
-     		}
-    	}	
-	}	
+  int x[3] = {5, 2, 1}, i;
+  while(1) {
+    scanf("%d %d %d", &x[0], &x[1], &x[2]);
+    for(i=0;i<2;i++) {
+      if( x[i] > x[i+1]) {
+  	    int temp = x[i];
+  	    x[i] = x[i+1];
+  	    x[i+1] = temp;
+      }
+      i = i + 1;
+      i = i - 1;
+    }
     printf(" %d, %d, %d", x[0], x[1], x[2]);
-	return 0;
+  }
+  return 0;
 }
